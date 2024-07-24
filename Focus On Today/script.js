100f501163784d700dba00d21067a70e7c507cb3
Iqra327 <iqrarasheed219@gmail.com>
2024-07-25 00:24:28 +0500

    initial-commit

--- Focus On Today/script.js
+++ Focus On Today/script.js
@@ -1,22 +1,41 @@
-const inputId = document.querySelector('.js-input-area');
-const radioBtn = document.querySelector('.js-radio-button');
+// const inputId = document.querySelector('.js-input-area');
+// const checkbox = document.querySelector('.js-checkbox');
 
-function handleInputs(){
+// function handleInputs(){
 
-  radioBtn.addEventListener('click', () => {
-    if(inputId.value.trim() === ''){
-      document.querySelector('.js-error-msg').classList.add('error-msg-display');
-      document.querySelector('.js-error-msg').classList.remove('error-msg');
-      radioBtn.checked = false;
-    }
-    else{
-      document.querySelector('.js-error-msg').classList.add('error-msg');
-      document.querySelector('.input-area').classList.add('input-area-mark');
-      localStorage.setItem('input', JSON.stringify(inputId.value.trim()));
-    }
+//   checkbox.addEventListener('click', () => {
+//     if(inputId.value.trim() === ''){
+//       document.querySelector('.js-error-msg').classList.add('error-msg-display');
+//       document.querySelector('.js-error-msg').classList.remove('error-msg');
+//       radioBtn.checked = false;
+//     }
+//     else{
+//       document.querySelector('.js-error-msg').classList.add('error-msg');
+//       document.querySelector('.input-div').classList.add('completed-task');
+//       localStorage.setItem('input', JSON.stringify(inputId.value.trim()));
+//     }
     
-  })
-}
-handleInputs();
-handleInputs();
-handleInputs();
+//   })
+// }
+// handleInputs();
+// handleInputs();
+// handleInputs();
+
+// const inputId = document.querySelectorAll('.js-input-area');
+// const checkboxList = document.querySelectorAll('.js-checkbox');
+
+// checkboxList.forEach((checkbox) => {
+//   checkbox.addEventListener('click', () => {
+//     if(inputId.value === ''){
+//       checkbox.parentElement.classList.toggle('completed-task');
+      
+//     }
+//     else
+//     {
+//       document.querySelector('.js-error-msg').classList.add('error-msg-display'); 
+//       document.querySelector('.js-error-msg').classList.remove('error-msg'); 
+
+//     }
+           
+//   })
+// })
